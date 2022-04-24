6a75aa1fd9032bf5b6f1d76e8b2a72ce0d2bdec5
egg <eggboissaye@gmail.com>
2022-04-24 14:58:20 -0400

    Initial commit

---
+++ src/main/java/me/eggerga/itemrace/ItemRace.java
@@ -0,0 +1,17 @@
+package me.eggerga.itemrace;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ItemRace extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
